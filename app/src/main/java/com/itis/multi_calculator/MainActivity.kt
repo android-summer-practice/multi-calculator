a0d21081e3f5209db1deb951367a893657ec44fc
nice-paradise <Ilnurkashapov2938@gmail.com>
2026-07-09 19:54:26 +0300

    откатил MainActivity

--- app/src/main/java/com/itis/multi_calculator/MainActivity.kt
+++ app/src/main/java/com/itis/multi_calculator/MainActivity.kt
@@ -3,25 +3,45 @@ package com.itis.multi_calculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.itis.multi_calculator.calculator.CalculatorScreen
+import androidx.compose.ui.tooling.preview.Preview
 import com.itis.multi_calculator.ui.theme.MulticalculatorTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             MulticalculatorTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    CalculatorScreen()
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MulticalculatorTheme {
+        Greeting("Android")
     }
 }
