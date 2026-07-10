b0545fe38a1a7ad4b39f63f784a13964c076c795
askar <askareliseev012007@mail.ru>
2026-07-10 22:05:47 +0300

    Добавлена навигация для конвертера, и конвертер подключен к истории

--- app/src/main/java/com/itis/multi_calculator/MainActivity.kt
+++ app/src/main/java/com/itis/multi_calculator/MainActivity.kt
@@ -9,6 +9,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.itis.multi_calculator.calculator.CalculatorScreen
+import com.itis.multi_calculator.calculator.MenuScreen
+import com.itis.multi_calculator.history.HistoryScreen
+import com.itis.multi_calculator.si_converter.SiConverterScreen
 import com.itis.multi_calculator.ui.theme.MulticalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,8 +24,34 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MulticalculatorTheme {
+                val navController = rememberNavController()
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    VectorsScreen(modifier = Modifier.padding(innerPadding))
+                    NavHost(
+                        navController = navController,
+                        startDestination = "menu",
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+                        composable("menu") {
+                            MenuScreen(
+                                onNavigateToCalculator = { navController.navigate("calculator") },
+                                onNavigateToVectors = { navController.navigate("vectors") },
+                                onNavigateToSi = { navController.navigate("si_converter") },
+                                onNavigateToHistory = { navController.navigate("history") }
+                            )
+                        }
+                        composable("calculator") {
+                            CalculatorScreen(onMenuClick = { navController.popBackStack() })
+                        }
+                        composable("vectors") {
+                            VectorsScreen(onMenuClick = { navController.popBackStack() })
+                        }
+                        composable("si_converter") {
+                            SiConverterScreen(onMenuClick = { navController.popBackStack() })
+                        }
+                        composable("history") {
+                            HistoryScreen(onMenuClick = { navController.popBackStack() })
+                        }
+                    }
                 }
             }
         }
